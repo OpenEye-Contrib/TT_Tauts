ebafd0ab84a559ac1602e0e5340275692fc41d52
DavidACosgrove <davidacosgroveaz@gmail.com>
2016-12-22 16:58:11 +0000

    Fixed path to exe for testing.

--- test_dir/test_tt_tauts_batch.sh
+++ test_dir/test_tt_tauts_batch.sh
@@ -2,6 +2,6 @@
 
 for smi_file in `ls [0-9]*.smi agl[12].smi chebi2295_small.smi` ; do
 	echo "XXXXXXXXXXXXX" , $smi_file
-	/home/cosgrove/SVN_WORK/TT_Tauts/exe_DEBUG/tt_tauts_batch $smi_file | grep -e "AWOOGA" -e "dodgy SMILES"
+	../src/exe_Debug/tt_tauts_batch $smi_file | grep -e "AWOOGA" -e "dodgy SMILES"
 	echo "YYYYYYYYYYYYY"
 done
